521bba554d17e159b6d5e0a33e74b75a5250b1c8
anodaz <aze123AZE>
2018-03-17 18:59:26 +0100

    good

--- app/src/main/java/dz/univoran/amd/activities/LoginActivity.java
+++ app/src/main/java/dz/univoran/amd/activities/LoginActivity.java
@@ -6,10 +6,12 @@ import android.annotation.TargetApi;
 import android.app.LoaderManager;
 import android.app.ProgressDialog;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -22,6 +24,16 @@ import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -60,6 +72,118 @@ public class LoginActivity extends AppCompatActivity implements LoaderManager.Lo
     }
 
 
+    public void nextPage(String data){
+        try {
+            JSONObject obj = new JSONObject(data);
+            if (!obj.getString("id").equals("0")){
+                Intent i = new Intent(this, Home.class);
+                Bundle b=new Bundle();
+                b.putString("user",obj.getString("user"));
+                b.putString("pass",obj.getString("pass"));
+                b.putString("id",obj.getString("id"));
+                i.putExtras(b);
+                startActivity(i);
+                this.finish();
+            }else {
+                stat.setText("The username or password is incorrect.");
+
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+    public class MyAsyncTaskgetNews extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+
+            // String url="https://api.ipify.org?format=json";
+            //     stat.setText(url);
+
+        }
+        @Override
+        protected String  doInBackground(String... params) {
+            StringBuilder sb = new StringBuilder();
+
+            //String http = "http://android.schoolportal.gr/Service.svc/SaveValues";
+
+
+            HttpURLConnection urlConnection=null;
+            try {
+                URL url = new URL(params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setUseCaches(false);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setRequestProperty("Content-Type","application/json");
+
+                //  urlConnection.setRequestProperty("Host", "android.schoolportal.gr");
+                urlConnection.connect();
+
+                //Create JSONObject here
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("id", "1");
+                jsonParam.put("user", user.getText());
+                jsonParam.put("pass", pass.getText());
+                OutputStreamWriter out = new OutputStreamWriter(urlConnection.getOutputStream());
+                out.write(jsonParam.toString());
+                out.close();
+
+                int HttpResult =urlConnection.getResponseCode();
+                if(HttpResult ==HttpURLConnection.HTTP_OK){
+                    BufferedReader br = new BufferedReader(new InputStreamReader(
+                            urlConnection.getInputStream(),"utf-8"));
+                    String line = null;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line + "\n");
+                    }
+                    br.close();
+
+                    publishProgress(sb.toString());
+
+                }else{
+                    publishProgress(urlConnection.getResponseMessage());
+                }
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+
+                e.printStackTrace();
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }finally{
+                if(urlConnection!=null)
+                    urlConnection.disconnect();
+            }
+            return null;
+        }
+        protected void onProgressUpdate(String... progress) {
+
+            try {
+
+                //    stat.setText(progress[0]);
+                nextPage(progress[0]);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        }
+        protected void onPostExecute(String  result2){
+
+        }
+
+
+
+
+    }
 
 
 
